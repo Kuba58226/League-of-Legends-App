f90cdf9af277519b86813b67068e7dd28fbafb11
Kuba58226 <kubaozut@gmail.com>
2021-11-24 12:58:40 +0100

    [f/e] Handled Login view

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, {useEffect, useImperativeHandle, useState} from 'react'
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Login from './views/Login.js'
 import Home from './views/Home.js'
@@ -6,18 +7,25 @@ import Summoner from './views/Summoner.js'
 import TierList from './views/TierList.js'
 import Champions from './views/Champions.js'
 import Champion from './views/Champion.js'
+import {contextObject,AppContext} from './AppContext';
 
 function App() {
+  const [loggedIn,setLoggedIn] = useState(contextObject.isUserLogged)
+  const [token,setToken] = useState(contextObject.jwtToken)
+  const [role,setAccountType] = useState(contextObject.accountType)
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="login" element={<Login />}/>
-        <Route path="summoner/:summonerName" element={<Summoner />}/>
-        <Route path="tier-list/:gameType/:gameMode/:lane/:role" element={<TierList />}/>
-        <Route path="champions" element={<Champions />}/>
-        <Route path="champion/:champion" element={<Champion />}/>
-        <Route path="" element={<Home />}/>
-      </Routes>
+      <AppContext.Provider value={{isUserLogged:loggedIn,toggleLoggedState:setLoggedIn,jwtToken:token,toggleTokenState:setToken,userRole:role,toggleRoleState:setAccountType}}>
+        <Routes>
+          <Route path="login" element={<Login />}/>
+          <Route path="summoner/:summonerName" element={<Summoner />}/>
+          <Route path="tier-list/:gameType/:gameMode/:lane/:role" element={<TierList />}/>
+          <Route path="champions" element={<Champions />}/>
+          <Route path="champion/:champion" element={<Champion />}/>
+          <Route path="" element={<Home />}/>
+        </Routes>
+      </AppContext.Provider>
     </BrowserRouter>
   );
 }
--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, {useContext, useEffect, useImperativeHandle, useState} from 'react'
+import {Navigate} from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -13,116 +14,130 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import {AppContext} from '../AppContext';
+import Copyright from './../components/Copyright';
 
 const theme = createTheme();
 
 export default function Login() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+  const {Website} = require('../config/website.js');
+  const {isUserLogged,toggleLoggedState,jwtToken,toggleTokenState,userRole,toggleRoleState} = useContext(AppContext)
+
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+
+  function handleOnChangeEmail(e){
+    setEmail(e.target.value)
+  }
+
+  function handleOnChangePassword(e){
+    setPassword(e.target.value)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    fetch(`${Website.serverName}api/auth/login`,{method: "POST", headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'},
+    body: JSON.stringify({email: email, password: password})})
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      toggleLoggedState(true)
+      toggleTokenState(data.access_token)
+      toggleRoleState(data.user.role)
+      console.log(isUserLogged)
+    })
+  }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: 'url(https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2019/11/10/9dad23f6-2e82-45d8-a39c-232a7d2e611f/league-of-legends-world-championship-2019-finals-crowd)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
+    <div>
+      {!isUserLogged ?
+      <ThemeProvider theme={theme}>
+        <Grid container component="main" sx={{ height: '100vh' }}>
+          <CssBaseline />
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
             sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              backgroundImage: 'url(https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2019/11/10/9dad23f6-2e82-45d8-a39c-232a7d2e611f/league-of-legends-world-championship-2019-finals-crowd)',
+              backgroundRepeat: 'no-repeat',
+              backgroundColor: (t) =>
+                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
             }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Zaloguj się
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Zapamiętaj hasło"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
+          />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
                 Zaloguj się
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Zapomniałeś hasła?
-                  </Link>
+              </Typography>
+              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                <TextField onChange={handleOnChangeEmail}
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField onChange={handleOnChangePassword}
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Zapamiętaj hasło"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Zaloguj się
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Zapomniałeś hasła?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="#" variant="body2">
+                      {"Nie masz konta? Zarejestruj się"}
+                    </Link>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Nie masz konta? Zarejestruj się"}
-                  </Link>
-                </Grid>
-              </Grid>
-              <Copyright sx={{ mt: 5 }} />
+                <Copyright sx={{ mt: 5 }} />
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </ThemeProvider>
+      </ThemeProvider>
+      : <Navigate to="/" />}
+    </div>
   );
 }
